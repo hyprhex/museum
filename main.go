468dbce02cd6112c055d9a957e8604523ca00ebf
HyprHex <swapapis@gmail.com>
2023-12-21 20:00:16 +0100

    Create a go template

--- main.go
+++ main.go
@@ -3,19 +3,31 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"text/template"
 )
 
 func handleHello(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from Go file"))
 }
 
+func handleTemplate(w http.ResponseWriter, r *http.Request) {
+	html, err := template.ParseFiles("templates/index.templ")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Internal server error"))
+		return
+	}
+	
+	html.Execute(w, "Test")
+}
+
 func main() {
 	server := http.NewServeMux()
 	server.HandleFunc("/hello", handleHello)
+	server.HandleFunc("/template", handleTemplate)
 
 	fs := http.FileServer(http.Dir("./public"))
 	server.Handle("/", fs)
-
 
 	err := http.ListenAndServe(":8888", server)
 	if err != nil {
