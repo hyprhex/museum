abfb5107c4e0bc156d43ec056854a95099215a11
HyprHex <swapapis@gmail.com>
2023-12-21 19:08:25 +0100

    Create a local http server with Hello route

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func main() {
+    http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+        w.Write([]byte("Hello from Go file"))
+    })
+    err := http.ListenAndServe(":3333", nil)
+    if err != nil {
+        fmt.Println("Error while opening the server")
+    }
 }
